fda50b545e75a6faa78791c19936a0213f21d1fa
Darian1996 <1934849492@qq.com>
2024-10-27 18:59:37 +0800

    2024-10-27 18:59:37 AutoCommit.sh by Darian

---
+++ src/main/java/cn/nogeek/controller/AService.java
@@ -0,0 +1,20 @@
+package cn.nogeek.controller;
+
+import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.util.List;
+
+/***
+ *
+ *
+ * @author <a href="mailto:">notOnlyGeek</a>
+ * @date 2024/10/21  下午10:41
+ */
+@Service
+public class AService {
+
+    public String sayHello() {
+        return "noGeek.cn";
+    }
+}
---
+++ src/test/java/cn/nogeek/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nogeek;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
